598abb7f7468d3145762d73fbd8281d7a7b82eec
joaoleandro <new.joaoleandro@gmail.com>
2024-04-02 14:15:07 -0300

    feat: Limpando estrutura do projeto

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({ subsets: ["latin"], weight: ["400", "800"] });
+
+export const metadata: Metadata = {
+  title: "Add Links Websites",
+  description: "Adicionando links de WebSites",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  );
+}
